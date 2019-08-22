c2dba300693c614f06b6834704992aad2b1e6a48
itsdun1 <adityakalengp@gmail.com>
2019-08-23 02:30:09 +0530

    =bug_solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
     title: "JBL Go Portable Wireless Bluetooth Speaker with Mic (Black)",
     sourceCategory: "Bluetooth Speakers",
     product_id: "ABCD1234",
-    createdAt: "2018-08-31T09:36:08.448Z",
+    createdAt: new Date("2018-08-31T09:36:08.448Z"),
     reviewCount: "3013",
   }
 
@@ -24,9 +24,7 @@ export class AppComponent {
 
  ngOnInit(){
 
-  if(Object.keys(this.data).length > 0){
-    this.moDate = new Date(this.data.createdAt);
-  }
+
 
 
 
