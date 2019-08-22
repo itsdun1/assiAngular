a2730018ae2fef878cfaa0198797f509ae1995cc
itsdun1 <adityakalengp@gmail.com>
2019-08-23 03:13:21 +0530

    =d

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(`${__dirname}/front-end/dist/`));
 app.listen(process.env.PORT || 8080)
 
 app.get( '/*', (req,res)=>{
-  res.sendFile(`./front-end/dist/index.html`)
+  res.sendFile(`${__dirname}/front-end/dist/index.html`)
 })
 
 // path.join(`./front-end/dist/index.html`)
