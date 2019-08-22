8e0fe0044574aa38892543775d970332981bd44c
itsdun1 <adityakalengp@gmail.com>
2019-08-23 03:07:43 +0530

    =d

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require("express");
 var app = express()
 const path = require("path")
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/front-end/dist/`));
 
 app.listen(process.env.PORT || 8080)
 
 app.get( '/*', (req,res)=>{
-  res.sendFile(path.join(__dirname + '/dist/index.html'))
+  res.sendFile(`./front-end/dist/index.html`)
 })
+
+// path.join(`./front-end/dist/index.html`)
